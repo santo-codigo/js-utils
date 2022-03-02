5ab7c9f8c72cbb2909b6ce72f8649d4c242a9d41
Lucas Santos <lucassm02@gmail.com>
2022-03-01 23:13:40 -0300

    test: ensure repeatUntilSucceed returns null if timeout exceeded

---
+++ src/repeat-until-succeed/repeat-until-succeed.spec.ts
@@ -0,0 +1,19 @@
+import { repeatUntilSucceed } from './repeat-until-succeed';
+
+describe('repeatUntilSucceed', () => {
+  test('should return null if timeout exceeded', async () => {
+    const sut = repeatUntilSucceed;
+
+    const params = {
+      callback: async () => {
+        return { success: false, data: { name: 'test' } };
+      },
+      timeToSleep: 0,
+      timeout: 1,
+    };
+
+    const result = await sut(params);
+
+    expect(result).toBe(null);
+  });
+});
