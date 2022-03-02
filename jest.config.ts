2dbcf56a22a508b653c4105eb02eb484b43ca112
Lucas Santos <lucassm02@gmail.com>
2022-03-01 22:23:05 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  clearMocks: true,
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  preset: 'ts-jest',
+};
